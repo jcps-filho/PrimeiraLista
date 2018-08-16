0219bf6f9e37a1291a19c7b5e12a419edcb3376d
José Carlos <jcps.filho@gmail.com>
2018-08-15 22:20:49 -0300

    Envio da questão 3

--- src/Questao3/Questao3.java
+++ src/Questao3/Questao3.java
@@ -1,7 +1,18 @@
 package Questao3;
 
+import java.util.Scanner;
+
 public class Questao3 {
     public static void main(String[] args){
+        Scanner leitor = new Scanner(System.in);
+        int tabuada;
 
+        System.out.printf("Informe qual tabuada deseja vizualizar: ");
+        tabuada = leitor.nextInt();
+
+        for(int i = 0; i <= 10; i++){
+
+            System.out.println(tabuada+" x "+ i +" = "+ tabuada * i);
+        }
     }
 }
